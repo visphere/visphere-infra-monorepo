7eac7aba554fd6709299b5dd1252dd342140e43d
milosz08 <gilgamilosz451@gmail.com>
2023-09-02 16:53:14 +0200

    create init multi-module scaffold

---
+++ msph-account-service/src/main/java/pl/moonsphere/accountservice/AccountServiceEntrypoint.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <https://miloszgilga.pl>
+ * Silesian University of Technology
+ *
+ *     File name: Main.java
+ *     Last modified: 9/2/23, 4:31 PM
+ *     Project name: moonsphere-infra-monorepo
+ *     Module name: msph-account-service
+ *
+ * This project is a part of "MoonSphere" instant messenger system. This system is a part of
+ * completing an engineers degree in computer science at Silesian University of Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the \"License\"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+package pl.moonsphere.accountservice;
+
+import pl.moonsphere.lib.SharedLibEntryClass;
+
+public class AccountServiceEntrypoint {
+    public static void main(String[] args) {
+        final SharedLibEntryClass sharedLibEntryClass = new SharedLibEntryClass();
+        sharedLibEntryClass.printInfo();
+    }
+}
