d6e2b260481b2f4025ee84e0f0fc91f9e4a3557f
milosz08 <gilgamilosz451@gmail.com>
2023-09-03 15:04:14 +0200

    minor changes

--- msph-shared-lib/src/main/java/pl/moonsphere/lib/SharedLibEntryClass.java
+++ msph-shared-lib/src/main/java/pl/moonsphere/lib/SharedLibEntryClass.java
@@ -3,27 +3,27 @@
  * Silesian University of Technology
  *
  *     File name: SharedLibEntryClass.java
- *     Last modified: 9/2/23, 4:33 PM
+ *     Last modified: 9/2/23, 4:34 PM
  *     Project name: moonsphere-infra-monorepo
  *     Module name: msph-shared-lib
  *
  * This project is a part of "MoonSphere" instant messenger system. This system is a part of
  * completing an engineers degree in computer science at Silesian University of Technology.
  *
- * Licensed under the Apache License, Version 2.0 (the \"License\"); you may not use this
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
  * file except in compliance with the License. You may obtain a copy of the License at
  *
  *     <http://www.apache.org/license/LICENSE-2.0>
  *
  * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the license.
  */
 package pl.moonsphere.lib;
 
 public class SharedLibEntryClass {
-    public void printInfo() {
-        System.out.println("Shared LIB");
+    public String printInfo() {
+        return "Shared LIB";
     }
 }
