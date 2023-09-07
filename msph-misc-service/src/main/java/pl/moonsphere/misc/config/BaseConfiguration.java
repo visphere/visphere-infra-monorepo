dfb4c5171bb7deb281dcbfdb8cbd78626ef11ea5
milosz08 <gilgamilosz451@gmail.com>
2023-09-07 02:11:52 +0200

    minor changes

--- msph-misc-service/src/main/java/pl/moonsphere/misc/config/BaseConfiguration.java
+++ msph-misc-service/src/main/java/pl/moonsphere/misc/config/BaseConfiguration.java
@@ -20,7 +20,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the license.
  */
-
 package pl.moonsphere.misc.config;
 
 import org.springframework.context.annotation.Bean;
