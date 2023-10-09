0f55b1ca0c54f52dd4f68ad774b42405fbcd4b7e
milosz08 <gilgamilosz451@gmail.com>
2023-10-10 01:21:56 +0200

    create kafka SSL with SASL configuration

--- vsph-auth-service/src/main/java/pl/visphere/auth/AuthServiceEntrypoint.java
+++ vsph-auth-service/src/main/java/pl/visphere/auth/AuthServiceEntrypoint.java
@@ -7,7 +7,9 @@ package pl.visphere.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceEntrypoint {
