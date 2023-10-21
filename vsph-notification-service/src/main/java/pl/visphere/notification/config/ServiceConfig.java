400e94f84cfc851549f368fed9995e9c94c5c80b
milosz08 <gilgamilosz451@gmail.com>
2023-10-21 19:27:03 +0200

    add beans configuration into microservices

--- vsph-notification-service/src/main/java/pl/visphere/notification/config/ServiceConfig.java
+++ vsph-notification-service/src/main/java/pl/visphere/notification/config/ServiceConfig.java
@@ -2,21 +2,15 @@
  * Copyright (c) 2023 by Visphere & Vsph Technologies
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-package pl.visphere.misc.config;
+package pl.visphere.notification.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.web.client.RestTemplate;
 import pl.visphere.lib.jwt.JwtService;
 
 @Configuration
 class ServiceConfig {
-    @Bean
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
     @Bean
     JwtService jwtService(Environment environment) {
         return new JwtService(environment);
