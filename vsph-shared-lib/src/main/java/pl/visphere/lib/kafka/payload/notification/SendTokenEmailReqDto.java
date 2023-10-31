30ffacf7a63e7a27aaa25c789b253e80f7301488
milosz08 <gilgamilosz451@gmail.com>
2023-10-31 04:12:22 +0100

    minor changes

--- vsph-shared-lib/src/main/java/pl/visphere/lib/kafka/payload/notification/SendTokenEmailReqDto.java
+++ vsph-shared-lib/src/main/java/pl/visphere/lib/kafka/payload/notification/SendTokenEmailReqDto.java
@@ -2,14 +2,18 @@
  * Copyright (c) 2023 by Visphere & Vsph Technologies
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-package pl.visphere.lib.kafka.payload;
+package pl.visphere.lib.kafka.payload.notification;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.ZonedDateTime;
 
 @Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class SendTokenEmailReqDto {
     private String fullName;
