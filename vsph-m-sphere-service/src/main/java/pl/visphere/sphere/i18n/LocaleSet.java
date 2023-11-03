4e68aba5f90e42e61fbfa1d9774b909e03ba9fb9
milosz08 <gilgamilosz451@gmail.com>
2023-11-04 00:46:04 +0100

    add i18n keys for sphere microservice

--- vsph-m-sphere-service/src/main/java/pl/visphere/sphere/i18n/LocaleSet.java
+++ vsph-m-sphere-service/src/main/java/pl/visphere/sphere/i18n/LocaleSet.java
@@ -11,7 +11,19 @@ import pl.visphere.lib.i18n.LocaleExtendableSet;
 @Getter
 @RequiredArgsConstructor
 public enum LocaleSet implements LocaleExtendableSet {
-    ;
+    SPHERE_GUILD_CREATED_RESPONSE_SUCCESS("vsph.i18n.sphereGuildCreated.res.success"),
+    SPHERE_GUILD_UPDATE_NAME_RESPONSE_SUCCESS("vsph.i18n.sphereGuildUpdateName.res.success"),
+    SPHERE_GUILD_UPDATE_CATEGORY_RESPONSE_SUCCESS("vsph.i18n.sphereGuildUpdateCategory.res.success"),
+    SPHERE_GUILD_UPDATE_VISIBILITY_RESPONSE_SUCCESS("vsph.i18n.sphereGuildUpdateVisibility.res.success"),
+    SPHERE_GUILD_UPDATE_VISIBILITY_WITH_REMOVE_LINKS_RESPONSE_SUCCESS("vsph.i18n.sphereGuildUpdateVisibilityWithRemovedLinks.res.success"),
+    SPHERE_GUILD_LINK_CREATED_RESPONSE_SUCCESS("vsph.i18n.sphereGuildLinkCreated.res.success"),
+    SPHERE_GUILD_LINK_UPDATED_EXPIRATION_RESPONSE_SUCCESS("vsph.i18n.sphereGuildLinkUpdatedExpiration.res.success"),
+    SPHERE_GUILD_LINK_UPDATED_ACTIVE_RESPONSE_SUCCESS("vsph.i18n.sphereGuildLinkUpdatedActive.res.success"),
+    SPHERE_GUILD_LINK_DELETED_RESPONSE_SUCCESS("vsph.i18n.sphereGuildLinkDeleted.res.success"),
+    //
+    SPHERE_GUILD_BY_ID_NOT_FOUND_EXCEPTION_MESSAGE("vsph.sphere.exc.sphereGuildByIdNotFound"),
+    SPHERE_GUILD_LINK_BY_ID_NOT_FOUND_EXCEPTION_MESSAGE("vsph.sphere.exc.sphereGuildLinkByIdNotFound"),
+    SPHERE_GUILD_LINK_INCORRECT_TIME_EXCEPTION_MESSAGE("vsph.sphere.exc.sphereGuildLinkIncorrectTime");
 
     private final String holder;
 }
