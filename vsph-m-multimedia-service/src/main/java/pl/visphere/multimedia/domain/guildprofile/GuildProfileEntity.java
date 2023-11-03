9cd1dfdefcec9d5f2a8035d55b912b69dc9a3f9b
milosz08 <gilgamilosz451@gmail.com>
2023-11-04 00:48:38 +0100

    create guild profile entity

---
+++ vsph-m-multimedia-service/src/main/java/pl/visphere/multimedia/domain/guildprofile/GuildProfileEntity.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+package pl.visphere.multimedia.domain.guildprofile;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import pl.visphere.lib.AbstractAuditableEntity;
+import pl.visphere.multimedia.domain.ImageType;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "guild_profiles")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuildProfileEntity extends AbstractAuditableEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String profileColor;
+
+    private String profileImageUuid;
+
+    @Enumerated(EnumType.STRING)
+    private ImageType imageType;
+
+    private Long guildId;
+
+    public String getProfileColor() {
+        return profileColor;
+    }
+
+    void setProfileColor(String profileColor) {
+        this.profileColor = profileColor;
+    }
+
+    public String getProfileImageUuid() {
+        return profileImageUuid;
+    }
+
+    public void setProfileImageUuid(String profileImageUuid) {
+        this.profileImageUuid = profileImageUuid;
+    }
+
+    public ImageType getImageType() {
+        return imageType;
+    }
+
+    void setImageType(ImageType imageType) {
+        this.imageType = imageType;
+    }
+
+    Long getGuildId() {
+        return guildId;
+    }
+
+    void setGuildId(Long guildId) {
+        this.guildId = guildId;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            "profileColor=" + profileColor +
+            ", profileImageUuid=" + profileImageUuid +
+            ", imageType=" + imageType +
+            ", guildId=" + guildId +
+            '}';
+    }
+}
---
+++ vsph-m-multimedia-service/src/main/java/pl/visphere/multimedia/exception/GuildProfileException.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+package pl.visphere.multimedia.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import pl.visphere.lib.LibLocaleSet;
+import pl.visphere.lib.exception.AbstractRestException;
+
+public class GuildProfileException {
+    @Slf4j
+    public static class GuildProfileNotFoundException extends AbstractRestException {
+        public GuildProfileNotFoundException(Long guildId) {
+            super(HttpStatus.NOT_FOUND, LibLocaleSet.GUILD_PROFILE_NOT_FOUND_EXCEPTION_MESSAGE);
+            log.error("Searching guild profile for guild id: '{}' not found in database", guildId);
+        }
+    }
+}
