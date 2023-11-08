9b73a8f83843ff93d98498f058874a9a152a0aee
milosz08 <gilgamilosz451@gmail.com>
2023-11-08 02:59:39 +0100

    move cdn property into separate file

--- vsph-shared-lib/src/main/java/pl/visphere/lib/s3/CdnProperty.java
+++ vsph-shared-lib/src/main/java/pl/visphere/lib/s3/CdnProperty.java
@@ -4,28 +4,21 @@
  */
 package pl.visphere.lib.s3;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
-import pl.visphere.lib.GenericProperty;
+import pl.visphere.lib.Property;
 
+@Getter
 @RequiredArgsConstructor
-enum S3Property implements GenericProperty {
-    URL("url"),
-    ACCESS_KEY("access-key"),
-    SECRET_KEY("secret-key"),
-    REGION("region"),
-    PATH_STYLE_ACCESS_ENABLED("path-style-access-enabled"),
+public enum CdnProperty implements Property {
+    CDN_BASE_URL("cdn-base-url"),
     ;
-
+    
     private final String value;
 
     @Override
     public String getValue(Environment environment) {
         return environment.getProperty("visphere.s3." + value);
-    }
-
-    @Override
-    public <T> T getValue(Environment environment, Class<T> type) {
-        return environment.getProperty("visphere.s3." + value, type);
     }
 }
