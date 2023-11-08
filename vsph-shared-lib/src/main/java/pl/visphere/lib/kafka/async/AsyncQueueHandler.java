a41315a4e13a19c053859940ce48b2fd057531b3
milosz08 <gilgamilosz451@gmail.com>
2023-11-08 02:48:11 +0100

    rewrite async queue handler for locale interchange support

---
+++ vsph-shared-lib/src/main/java/pl/visphere/lib/kafka/async/AsyncQueueHandler.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+package pl.visphere.lib.kafka.async;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.SerializationUtils;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.KafkaTemplate;
+import pl.visphere.lib.kafka.QueueTopic;
+
+import java.util.Locale;
+import java.util.UUID;
+
+import static pl.visphere.lib.kafka.sync.SyncQueueHandler.LOCALE_HEADER;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AsyncQueueHandler {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final Environment environment;
+
+    public void sendAsyncWithNonBlockingThread(QueueTopic topic, Object data) {
+        final String key = UUID.randomUUID().toString();
+        final String decodedTopic = topic.getValue(environment);
+        final Locale currentLocale = LocaleContextHolder.getLocale();
+
+        log.info("Started async kafka call into '{}' with key: '{}' and data: '{}'", decodedTopic, key, data);
+
+        final ProducerRecord<String, Object> record = new ProducerRecord<>(decodedTopic, key, data);
+        record.headers().add(LOCALE_HEADER, SerializationUtils.serialize(currentLocale));
+
+        kafkaTemplate.send(record);
+        log.info("End async kafka call into '{}'", decodedTopic);
+    }
+}
