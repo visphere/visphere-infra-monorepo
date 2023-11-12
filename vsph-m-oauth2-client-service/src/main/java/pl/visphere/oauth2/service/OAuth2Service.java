8935de7878b10a2e1d821111702a2b405f934929
milosz08 <gilgamilosz451@gmail.com>
2023-11-12 01:42:28 +0100

    initialized oauth2 flow service

---
+++ vsph-m-oauth2-client-service/src/main/java/pl/visphere/oauth2/service/OAuth2Service.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+package pl.visphere.oauth2.service;
+
+public interface OAuth2Service {
+}
---
+++ vsph-m-oauth2-client-service/src/main/java/pl/visphere/oauth2/service/OAuth2ServiceImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+package pl.visphere.oauth2.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pl.visphere.oauth2.core.user.OAuth2UserInfo;
+import pl.visphere.oauth2.core.user.OAuth2UserLoader;
+import pl.visphere.oauth2.core.user.OAuth2UserResponse;
+import pl.visphere.oauth2.core.user.UserLoaderData;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OAuth2ServiceImpl implements OAuth2Service, OAuth2UserLoader {
+    @Override
+    public OAuth2UserResponse loadAndAuthUser(UserLoaderData userLoaderData, OAuth2UserInfo userInfo) {
+        // TODO: login or register oauth2 user
+
+        System.out.println(userLoaderData);
+        System.out.println(userInfo);
+
+        return OAuth2UserResponse.builder()
+            .userDetails(null)
+            .isAlreadySignup(false)
+            .build();
+    }
+}
