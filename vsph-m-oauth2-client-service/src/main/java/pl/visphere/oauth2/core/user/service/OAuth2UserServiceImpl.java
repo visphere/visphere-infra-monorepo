c3ac45e1b586e9e0eda17179f3b870c75e8f3160
milosz08 <gilgamilosz451@gmail.com>
2023-11-14 01:05:35 +0100

    wrap service with try catch

--- vsph-m-oauth2-client-service/src/main/java/pl/visphere/oauth2/core/user/service/OAuth2UserServiceImpl.java
+++ vsph-m-oauth2-client-service/src/main/java/pl/visphere/oauth2/core/user/service/OAuth2UserServiceImpl.java
@@ -6,11 +6,13 @@ package pl.visphere.oauth2.core.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import pl.visphere.lib.exception.AbstractRestException;
 import pl.visphere.oauth2.core.OAuth2Properties;
 import pl.visphere.oauth2.core.OAuth2Supplier;
 import pl.visphere.oauth2.core.user.*;
@@ -24,27 +26,32 @@ public class OAuth2UserServiceImpl extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        final OAuth2User user = super.loadUser(userRequest);
+        try {
+            final OAuth2User user = super.loadUser(userRequest);
 
-        final OAuth2Supplier supplier = OAuth2Supplier
-            .checkIfSupplierExist(userRequest, oAuth2Properties.getSuppliers());
+            final OAuth2Supplier supplier = OAuth2Supplier
+                .checkIfSupplierExist(userRequest, oAuth2Properties.getSuppliers());
 
-        final UserLoaderData userLoaderData = UserLoaderData.builder()
-            .supplier(supplier)
-            .build();
+            final UserLoaderData userLoaderData = UserLoaderData.builder()
+                .supplier(supplier)
+                .build();
 
-        final OAuth2UserInfo userInfo = OAuth2UserInfo.OAuth2UserInfoFabricator
-            .fabricate(supplier, user.getAttributes());
+            final OAuth2UserInfo userInfo = OAuth2UserInfo.OAuth2UserInfoFabricator
+                .fabricate(supplier, user.getAttributes());
 
-        final OAuth2UserResponse response = oAuth2UserLoader.loadAndAuthUser(userLoaderData, userInfo);
-        final OAuth2UserData resData = OAuth2UserData.builder()
-            .userDetails(response.userDetails())
-            .userLoaderData(userLoaderData)
-            .attributes(user.getAttributes())
-            .isAlreadySignup(response.isAlreadySignup())
-            .build();
+            final OAuth2UserResponse response = oAuth2UserLoader.loadAndAuthUser(userLoaderData, userInfo);
+            final OAuth2UserData resData = OAuth2UserData.builder()
+                .openId(userInfo.getId())
+                .userDetails(response.userDetails())
+                .userLoaderData(userLoaderData)
+                .attributes(user.getAttributes())
+                .isAlreadySignup(response.isAlreadySignup())
+                .build();
 
-        log.info("Successfully load user from OAuth2 client: '{}'", resData);
-        return resData;
+            log.info("Successfully load user from OAuth2 client: '{}'", resData);
+            return resData;
+        } catch (AbstractRestException ex) {
+            throw new AuthenticationServiceException(ex.getPlaceholder().getHolder());
+        }
     }
 }
--- vsph-m-oauth2-client-service/src/main/java/pl/visphere/oauth2/core/user/service/OidcUserServiceImpl.java
+++ vsph-m-oauth2-client-service/src/main/java/pl/visphere/oauth2/core/user/service/OidcUserServiceImpl.java
@@ -6,11 +6,13 @@ package pl.visphere.oauth2.core.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
+import pl.visphere.lib.exception.AbstractRestException;
 import pl.visphere.oauth2.core.OAuth2Properties;
 import pl.visphere.oauth2.core.OAuth2Supplier;
 import pl.visphere.oauth2.core.user.*;
@@ -24,29 +26,34 @@ public class OidcUserServiceImpl extends OidcUserService {
 
     @Override
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
-        final OidcUser user = super.loadUser(userRequest);
+        try {
+            final OidcUser user = super.loadUser(userRequest);
 
-        final OAuth2Supplier supplier = OAuth2Supplier
-            .checkIfSupplierExist(userRequest, oAuth2Properties.getSuppliers());
+            final OAuth2Supplier supplier = OAuth2Supplier
+                .checkIfSupplierExist(userRequest, oAuth2Properties.getSuppliers());
 
-        final UserLoaderData userLoaderData = UserLoaderData.builder()
-            .supplier(supplier)
-            .oidcUserInfo(user.getUserInfo())
-            .oidcIdToken(user.getIdToken())
-            .build();
+            final UserLoaderData userLoaderData = UserLoaderData.builder()
+                .supplier(supplier)
+                .oidcUserInfo(user.getUserInfo())
+                .oidcIdToken(user.getIdToken())
+                .build();
 
-        final OAuth2UserInfo userInfo = OAuth2UserInfo.OAuth2UserInfoFabricator
-            .fabricate(supplier, user.getAttributes());
+            final OAuth2UserInfo userInfo = OAuth2UserInfo.OAuth2UserInfoFabricator
+                .fabricate(supplier, user.getAttributes());
 
-        final OAuth2UserResponse response = oAuth2UserLoader.loadAndAuthUser(userLoaderData, userInfo);
-        final OAuth2UserData resData = OAuth2UserData.builder()
-            .userDetails(response.userDetails())
-            .userLoaderData(userLoaderData)
-            .attributes(user.getAttributes())
-            .isAlreadySignup(response.isAlreadySignup())
-            .build();
+            final OAuth2UserResponse response = oAuth2UserLoader.loadAndAuthUser(userLoaderData, userInfo);
+            final OAuth2UserData resData = OAuth2UserData.builder()
+                .openId(userInfo.getId())
+                .userDetails(response.userDetails())
+                .userLoaderData(userLoaderData)
+                .attributes(user.getAttributes())
+                .isAlreadySignup(response.isAlreadySignup())
+                .build();
 
-        log.info("Successfully load user from OAuth2 (OIDC) client: '{}'", resData);
-        return resData;
+            log.info("Successfully load user from OAuth2 (OIDC) client: '{}'", resData);
+            return resData;
+        } catch (AbstractRestException ex) {
+            throw new AuthenticationServiceException(ex.getPlaceholder().getHolder());
+        }
     }
 }
