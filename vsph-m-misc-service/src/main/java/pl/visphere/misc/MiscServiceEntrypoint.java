698aa2c198c13a29848f11dcc8678d1dc0cadcbb
milosz08 <gilgamilosz451@gmail.com>
2023-12-27 09:56:31 +0100

    add missing EnableKafka annotation

--- vsph-m-misc-service/src/main/java/pl/visphere/misc/MiscServiceEntrypoint.java
+++ vsph-m-misc-service/src/main/java/pl/visphere/misc/MiscServiceEntrypoint.java
@@ -7,7 +7,9 @@ package pl.visphere.misc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MiscServiceEntrypoint {
